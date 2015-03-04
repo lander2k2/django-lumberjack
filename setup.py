459ae5002ab6ae8efa3b17d96535e69837cc60b3
Richard Lander <richard.lander@tribridge.com>
2015-03-04 09:24:00 -0500

    version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-lumberjack',
-      version='0.1',
+      version='0.2',
       description='Simple logging. Stored in database, accessed in Django admin.',
       url='http://github.com/lander2k2/django-lumberjack',
       author='Richard Lander',
